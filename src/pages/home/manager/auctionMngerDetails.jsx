dd45943a80f038798258ebf1364d7db0bfed6b84
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-12-18 02:06:40 +0700

    fix time auction

--- src/pages/home/manager/auctionMngerDetails.jsx
+++ src/pages/home/manager/auctionMngerDetails.jsx
@@ -150,15 +150,15 @@ const AuctionDetails = () => {
       const updatedData = {
         ...data.room,
         registrationOpenDate: values.registrationOpenDate
-          .subtract(7, "hours")
+          .subtract(0, "hours")
           .toISOString(),
         registrationEndDate: values.registrationEndDate
-          .subtract(7, "hours")
+          .subtract(0, "hours")
           .toISOString(),
         priceStep: values.priceStep,
         registrationFee: values.registrationFee,
-        activeDate: values.activeDate.subtract(7, "hours").toISOString(),
-        endDate: values.endDate.subtract(7, "hours").toISOString(),
+        activeDate: values.activeDate.subtract(0, "hours").toISOString(),
+        endDate: values.endDate.subtract(0, "hours").toISOString(),
         status: values.status,
         // modificationBy: values.modificationBy,
         modificationDate: new Date().toISOString(),
