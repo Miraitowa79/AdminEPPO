fe5e4cafd8801167708613f05f06f935f8c32212
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-12-17 20:42:43 +0700

    git push

--- src/pages/home/manager/auctionMngerDetails.jsx
+++ src/pages/home/manager/auctionMngerDetails.jsx
@@ -70,7 +70,11 @@ const AuctionDetails = () => {
 
         const accountDetails = await getAccountDetails(data.room.modificationBy);
         setAccount(accountDetails);
-
+        console.log('data-auction:', data)
+        console.log('log time:', registrationOpenDate)
+        console.log('log time:', registrationEndDate)
+        console.log('log time:', activeDate)
+        console.log('log time:', endDate)
       } catch (error) {
         console.error('Error fetching auction details:', error);
         message.error('Error fetching auction details');
@@ -118,12 +122,12 @@ const AuctionDetails = () => {
       const values = await form.validateFields();
       const updatedData = {
         ...data.room,
-        // registrationOpenDate: values.registrationOpenDate.add(7, 'hours').toISOString().local(),
-        // registrationEndDate: values.registrationEndDate.add(7, 'hours').toISOString().local(),
+        registrationOpenDate: values.registrationOpenDate.add(7, 'hours').toISOString(),
+        registrationEndDate: values.registrationEndDate.add(7, 'hours').toISOString(),
         priceStep: values.priceStep,
         registrationFee: values.registrationFee,
-        // activeDate: values.activeDate.add(7, 'hours').toISOString().local(),
-        // endDate: values.endDate.add(7, 'hours').toISOString().local(),
+        activeDate: values.activeDate.add(7, 'hours').toISOString(),
+        endDate: values.endDate.add(7, 'hours').toISOString(),
         status: values.status,
         // modificationBy: values.modificationBy,
         modificationDate: new Date().toISOString(),
@@ -208,6 +212,8 @@ const AuctionDetails = () => {
               value={moment()} 
             />
           </Form.Item>
+
+
           <Form.Item
             name="registrationEndDate"
             label="Ngày đóng đăng ký"
